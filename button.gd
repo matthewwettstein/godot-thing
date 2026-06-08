9c0fc1522cf84d7d811cac7ce50031a736a2f4be
matthewwettstein <matthew.wettstein@mcvts.org>
2026-06-08 09:16:37 -0400

    Add files via upload

--- button.gd
+++ button.gd
@@ -1,4 +1,4 @@
-extends Area2D
+extends Button
 
 
 # Called when the node enters the scene tree for the first time.
@@ -11,5 +11,5 @@ func _process(delta: float) -> void:
 	pass
 
 
-func _on_body_entered(body: Node2D) -> void:
-	get_tree().change_scene_to_file("res://win.tscn")
+func _on_pressed() -> void:
+	get_tree().change_scene_to_file("res://main.tscn")
